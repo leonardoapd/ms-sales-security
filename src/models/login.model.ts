20b10d79417300ef9b2d4ee7087449454bdee5d7
Leonardo Perdomo <leonardop763@gmail.com>
2023-08-30 15:43:28 -0400

    Feat (models)

---
+++ src/models/login.model.ts
@@ -0,0 +1,46 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Login extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  _id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  _2FACode: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  _2FACodeStatus: boolean;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  token: string;
+
+  @property({
+    type: 'boolean',
+    required: true,
+  })
+  tokenStatus: boolean;
+
+
+  constructor(data?: Partial<Login>) {
+    super(data);
+  }
+}
+
+export interface LoginRelations {
+  // describe navigational properties here
+}
+
+export type LoginWithRelations = Login & LoginRelations;
---
+++ src/models/role-menu.model.ts
@@ -0,0 +1,52 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class RoleMenu extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  _id?: string;
+
+  @property({
+    type: 'boolean',
+    required: true,
+  })
+  save: boolean;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  edit: boolean;
+
+  @property({
+    type: 'boolean',
+    required: true,
+  })
+  list: boolean;
+
+  @property({
+    type: 'boolean',
+    required: true,
+  })
+  delete: boolean;
+
+  @property({
+    type: 'boolean',
+    required: true,
+  })
+  download: boolean;
+
+
+  constructor(data?: Partial<RoleMenu>) {
+    super(data);
+  }
+}
+
+export interface RoleMenuRelations {
+  // describe navigational properties here
+}
+
+export type RoleMenuWithRelations = RoleMenu & RoleMenuRelations;
